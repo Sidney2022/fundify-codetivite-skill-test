4091f74faea6dfbe3f1252384e1ae935a1fa16a5
Sidney2022 <uwayasidney@gmail.com>
2024-11-07 16:04:35 +0100

    initial

---
+++ data.js
@@ -0,0 +1,14 @@
+let dummyData=[
+    {"name":"Dan Belo", "user_tag":"@shaffy", 
+        "txn_pin":7658, 'acct_balance':20098, "points":45098
+    },
+    {"name":"Shater Tsaver", "user_tag":"@shaffy", 
+        "txn_pin":7658, 'TsavarM':290098, "points":498
+    },
+    {"name":"Satar Mchiaga", "user_tag":"@MchiagaS", 
+        "txn_pin":7658, 'acct_balance':208, "points":4598
+    }
+]
+
+
+allowed_pins = ['4324', '8976', '8901', '8763', '2973', '1111']
